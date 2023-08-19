49c7e7c36dd14ecf4371d9ce05ede7f6f56b75f5
Madhav <mehta.madhav55@gmail.com>
2023-08-20 00:02:57 +0530

    1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,20 @@
-//This is home page, It will contains all the sections require in this page.
-
-//Import all the require sections here
+import React from "react";
+import styled from "styled-components";
 import HeroSection from "../Sections/Hero/index";
 import About from "../Sections/About/index";
 import Services from "../Sections/Services/index";
 import Testimonials from "../Sections/Testimonials/index";
 import Contact from "../Sections/Contact/index";
-import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  /* position: relative; */
+
+  @media (max-width: 768px) {
+    padding: 20px; /* Add some padding for better spacing on smaller screens */
+  }
 `;
 
 const Home = () => {
